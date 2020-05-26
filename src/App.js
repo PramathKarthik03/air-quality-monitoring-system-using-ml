acb44341367224616e0e3d836e030f8aaacf0ad4
PramathKarthik03 <48726752+PramathKarthik03@users.noreply.github.com>
2020-05-26 20:49:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+// import Home from "./components/Home";
+import Home from "./pages/Home";
+import AQI_Level from "./pages/AQI_Level";
+import AQIChart from "./pages/AQIChart";
+import FAQ from "./pages/FAQ";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/level" component={AQI_Level}></Route>
+      <Route path="/chart" component={AQIChart}></Route>
+      <Route path="/faq" component={FAQ}></Route>
+    </BrowserRouter>
+  );
+}
+
+export default App;
